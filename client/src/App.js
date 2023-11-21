257ca6d5bfdfc52e1792601d60bba8f5a4b87203
Jawad Rada <jawadrada@jawads-mbp.myfiosgateway.com>
2023-11-21 15:52:39 -0500

    src folder excluding the pages and components folders

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/Home';
+import LoginPage from './pages/Login';
+import AboutPage from './pages/About';
+import AlumDirectoryPage from './pages/AlumDirectory';
+import UserProfilePage from './pages/UserProfile.js';
+import './App.css';
+
+
+function App() {
+   return (
+       <Router>
+           <Routes>
+               <Route path="/" element={<HomePage />} />
+               <Route path="/directory" element={<AlumDirectoryPage />} />
+               <Route path="/profile" element={<UserProfilePage />} />
+               <Route path="/login" element={<LoginPage />} />
+               <Route path="/about" element={<AboutPage />} />
+           </Routes>
+       </Router>
+   );
+}
+
+export default App;
