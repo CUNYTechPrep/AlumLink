b289d4ed28e18feaac82e4924b9bbe1e7317f20d
Jawad Rada <jawadrada@Jawads-MacBook-Pro.local>
2023-11-30 12:02:58 -0500

    Add Login page with styling file

---
+++ client/src/pages/Login.js
@@ -0,0 +1,53 @@
+import React from "react";
+import "./LoginStyles.css";
+import Layout from "../components/Layout";
+import Button from "../components/Button";
+
+function Login() {
+  const handleClick = () => {
+    // this is where we will put what happens for each button click
+  };
+  const handleClick2 = () => {
+    // this is where we will put what happens for each button click
+  };
+
+  return (
+    <>
+      <Layout>
+        <div className="login-form">
+          <div className="">
+            <h1>Ready to get Started?</h1>
+            <p>
+              No need for a password. Just enter your email and we will log you
+              in magically!
+            </p>
+          </div>
+
+          <div className="email-box">
+            <label className="label-email" htmlFor="input-email">
+              Email:
+            </label>
+            <input
+              className="email-style"
+              type="email"
+              name="input-email"
+              id="input-email"
+              placeholder="email@myemail.com"
+            />
+          </div>
+
+          <div className="btn-div">
+            <Button text="Request Magic Link ✨" onClick={handleClick} />
+          </div>
+
+          <div>
+            <p>Don't have an account yet? Signing Up is a breeze!</p>
+            <Button text="Create an Account 🙌" onClick={handleClick2} />
+          </div>
+        </div>
+      </Layout>
+    </>
+  );
+}
+
+export default Login;
