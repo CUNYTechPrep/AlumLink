00e3ce93480ee6cfd97550ae0ed83306c7f92fcd
Mohamed Elsayed <78699419+M-Elsayed9@users.noreply.github.com>
2023-12-02 02:08:57 -0500

    Adding basic crud operations and end points

--- api/controllers/contactInfo.js
+++ api/controllers/contactInfo.js
@@ -1,4 +1,73 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../models");
-const { contactInfo } = db;
+const { Contact_infos } = db;
+
+router.post("/", createContactInfo);
+async function createContactInfo(req, res) {
+  try {
+    const contactInfo = req.body;
+    Contact_infos.create( contactInfo );
+    res.json(contactInfo);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+router.get("/", getContactInfos);
+async function getContactInfos(req, res) {
+  try {
+    const contactInfos = await Contact_infos.findAll();
+    res.json(contactInfos);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+router.get("/:id", getContactInfo);
+async function getContactInfo(req, res) {
+  try {
+    const contactInfo = await Contact_infos.findByPk(req.params.id);
+    res.json(contactInfo);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+router.put("/:id", updateContactInfo);
+async function updateContactInfo(req, res, next) {
+  try {
+    let contactInfo = req.body;
+    
+    const [updatedRowsCount, updatedContactInfos] = await Contact_infos.update(
+      { ...contactInfo },
+      { returning: true, where: { id: req.params.id } }
+    );
+
+    if (updatedRowsCount > 0) {
+      // If at least one row was updated, return the updated contactInfo
+      res.json(updatedContactInfos[0]); // Return the first updated contactInfo
+    } else {
+      res.status(404).json({ message: "ContactInfo not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+router.delete("/:id", deleteContactInfo);
+async function deleteContactInfo(req, res) {
+  try {
+    const contactInfo = await Contact_infos.findByPk(req.params.id);
+    if (contactInfo) {
+      await contactInfo.destroy();
+      res.json({ message: "ContactInfo deleted" });
+    } else {
+      res.status(404).json({ message: "ContactInfo not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = router;
--- api/controllers/experience.js
+++ api/controllers/experience.js
@@ -1,4 +1,70 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../models");
-const { experience } = db;
+const { Experiences } = db;
+
+router.post("/", createExperience);
+async function createExperience(req, res) {
+  try {
+    let experiencerInfo = req.body;
+    const experience = await Experiences.create(experiencerInfo );
+    res.json(experience);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+router.get("/", getExperiences);
+async function getExperiences(req, res) {
+  try {
+    const experiences = await Experiences.findAll();
+    res.json(experiences);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+router.get("/:id", getExperience);
+async function getExperience(req, res) {
+  try {
+    const experience = await Experiences.findByPk(req.params.id);
+    res.json(experience);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+router.put("/:id", updateExperience);
+async function updateExperience(req, res, next) {
+  try {
+    const experienceInfo = req.body;
+    
+    const [updatedRowsCount, updatedExperiences] = await Experiences.update(
+      { ...experienceInfo },
+      { returning: true, where: { id: req.params.id } }
+    );
+
+    if (updatedRowsCount > 0) {
+      // If at least one row was updated, return the updated experience
+      res.json(updatedExperiences[0]); // Return the first updated experience
+    } else {
+      res.status(404).json({ message: "Experience not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+router.delete("/:id", deleteExperience);
+async function deleteExperience(req, res) {
+  try {
+    await Experiences.destroy({
+      where: { id: req.params.id },
+    });
+    res.json({ message: "Deleted Experince" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = router;
