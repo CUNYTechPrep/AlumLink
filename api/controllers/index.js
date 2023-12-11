937f195b7bb07d81f7a79b015a10c28453b6a699
Mohamed Elsayed <78699419+M-Elsayed9@users.noreply.github.com>
2023-12-10 23:42:44 -0500

    add auth controller

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const contactInfoController = require("../controllers/contactInfo.js");
+const contactInfoController = require("./contactInfo.js");
 const employerController = require("./employer.js");
 const experienceController = require("./experience.js");
 const userController = require("./user.js");
+const authController = require("./auth.js");
 
 router.use("/contactInfo", contactInfoController);
 router.use("/employer", employerController);
 router.use("/experience", experienceController);
 router.use("/user", userController);
+router.use("/auth", authController);
+
 module.exports = router;
