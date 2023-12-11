6c7a6511cd59f5d0422f27f07fcc7fc12d0e8153
Mohamed Elsayed <78699419+M-Elsayed9@users.noreply.github.com>
2023-12-10 23:47:55 -0500

    refactor/reorganise main app

--- api/app.js
+++ api/app.js
@@ -1,23 +1,14 @@
 const express = require("express");
-const morgan = require("morgan");
-const { Sequelize } = require("sequelize");
-const path = require("path");
-const db = require("./models");
-const routes = require("./controllers");
 const app = express();
-require("dotenv").config();
+const path = require("path");
+const routes = require("./controllers");
+const middleware = require("./middlewares");
+const connectToDatabase = require("./config/database");
 const PORT = process.env.PORT || 8080;
 
-app.use(express.json());
-app.use("/api", require("./controllers"));
-// this mounts controllers/index.js at the route `/api`
-const logFormat = process.env.NODE_ENV === "production" ? "combined" : "dev";
-app.use(morgan(logFormat));
-
-const sequelize = new Sequelize('alumlinkdb', 'postgres', 'password', {
-  host: 'localhost',
-  dialect: 'postgres'
-});
+app.use(middleware);
+app.use("/api", routes); // this mounts controllers/index.js at the route /api
+connectToDatabase();
 
 // for production use, we serve the static react build folder
 if (process.env.NODE_ENV === "production") {
@@ -28,27 +19,11 @@ if (process.env.NODE_ENV === "production") {
   });
 }; 
  
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-// update DB tables based on model updates. Does not handle renaming tables/columns
-// NOTE: toggling this to true drops all tables (including data)
-//db.sequelize.sync({ force: false });
- 
 const startServer = async () => {
   try {
-    await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
-    
     app.listen(PORT, () => console.log(`Listening on ${PORT}`));
   } catch (error) {
-    console.error("Unable to connect to the database:", error);
+    console.error("Unable to connect to start the server", error);
   }
 };
 startServer();
-
-
-
-
-
